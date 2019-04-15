e592b6ea652e3242a212bf79f12b19ab920c9ef9
LukeHxH <lucasmed812@gmail.com>
2019-04-15 13:23:10 -0300

    Some refactoring

--- server/server.js
+++ server/server.js
@@ -3,18 +3,24 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 
+console.log('Estabelecendo conexão...');
+
+// Conexão ao banco de dados
 mongoose.connect('mongodb+srv://dev:lalala123@graphqlexample-aisry.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('Conectado ao banco de dados.');
-})
-
-const app = express();
-
-app.use('/api', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
-
-app.listen(4000, () => {
-    console.log('Executando na porta 4000...');
+    startConnection();
 });
+
+function startConnection() {
+    const app = express();
+
+    app.use('/api', graphqlHTTP({
+        schema,
+        graphiql: true
+    }));
+
+    app.listen(4000, () => {
+        console.log('Executando na porta 4000...');
+    });
+}
